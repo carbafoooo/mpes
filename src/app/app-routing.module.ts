a68d1dcad938bd40ca47e92bc6a1ba4070ee5363
unknown <xsi@IT-MAN.krm.local>
2020-02-24 14:14:08 +0330

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./layout/dashboard/dashboard.component";
+import { LoginComponent } from "./layout/login/login.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "dashboard", component: DashboardComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core'; 
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NzButtonModule } from 'ng-zorro-antd/button'
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NgZorroAntdModule } from "ng-zorro-antd";
+import { DashboardComponent } from "./layout/dashboard/dashboard.component";
+import { RegisterFormComponent } from './components/forms/register-form/register-form.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
+import { LoginComponent } from './layout/login/login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NzButtonModule
-  ],
+  declarations: [AppComponent, DashboardComponent, RegisterFormComponent, LoginFormComponent, LoginComponent],
+  imports: [BrowserModule, AppRoutingModule, NgZorroAntdModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
